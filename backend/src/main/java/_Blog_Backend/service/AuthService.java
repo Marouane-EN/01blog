4db6ebffd5a4690c8c49e07e183db170bd25217a
asaadane <>
2026-04-30 18:08:10 +0100

    feat: implement authentication service with user registration and security configuration

---
+++ backend/src/main/java/_Blog_Backend/service/AuthService.java
@@ -0,0 +1,41 @@
+package _Blog_Backend.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import _Blog_Backend.entity.User;
+import _Blog_Backend.repository.UserRepository;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User RegisterLocalUser(String username, String email, String password) {
+        if (userRepository.existsByUsername(username)) {
+            throw new RuntimeException("Error: Username is already taken!");
+        }
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Error: Email is already in use!");
+        }
+        String hashedPassword = passwordEncoder.encode(password);
+
+        User newUser = User.builder()
+                .username(username)
+                .email(email)
+                .passwordHash(hashedPassword)
+                .authProvider("LOCAL")
+                .role("USER")
+                .isActive(true)
+                .isBlocked(false)
+                .build();
+
+        return userRepository.save(newUser);
+
+    }
+}
