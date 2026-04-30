3d74c032b7068221cb5a23cb95741d9dff91ed2a
Marouane-EN <173193132+Marouane-EN@users.noreply.github.com>
2026-04-30 18:08:10 +0100

    feat: implement authentication service with user registration and security configuration

---
+++ backend/src/main/java/_Blog_Backend/controller/LoginController.java
@@ -0,0 +1,10 @@
+package _Blog_Backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginController {
+    
+}
